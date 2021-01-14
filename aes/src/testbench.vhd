0c0aa9f37d404f96f9cd375e4c8689526ab048db
Fatih <fatih.balli@epfl.ch>
2021-01-14 15:44:01 +0100

    Added makefile for AES

--- aes/src/testbench.vhd
+++ aes/src/testbench.vhd
@@ -19,10 +19,11 @@ architecture tb of testbench is
 	constant clkphase: time:= 50 ns;
 	constant quarter : time := 25 ns;
 	
-	file infile, ansfile, outfile : TEXT;	
+	file testinput, testoutput, tb_output : TEXT;	
 		
 	signal KeyBit, PTBit, CTBit, ClkxC, Reset, Ready: std_logic;	
 	constant total_cycles : integer := 128*11;
+	signal cont_flag : boolean := true;  
 	
 	component AES 
        port ( 
@@ -41,27 +42,44 @@ begin
 
 	process
 	begin
-		ClkxC <= '1'; wait for clkphase;
-		ClkxC <= '0'; wait for clkphase;
+		if cont_flag then
+            ClkxC <= '1';
+            wait for clkphase;
+            ClkxC <= '0';
+            wait for clkphase;
+        else
+            wait;
+        end if;
 	end process;
 	
 	process
 	begin
-		Reset <= '0'; wait for quarter;
-		Reset <= '1'; wait for 2*total_cycles*clkphase - quarter;
+		if cont_flag then
+			Reset <= '0'; wait for quarter;
+			Reset <= '1'; wait for 2*total_cycles*clkphase - quarter;
+		else
+			wait;
+		end if;
 	end process;
 
 	process
-		variable INLine : line;
+		variable line_var : line;
 		variable pt128, ct128, key128, tmp : std_logic_vector(127 downto 0);
+		variable counter  : integer;
 	begin
-		file_open(infile, "PTKEY", read_mode);
+		cont_flag <= true;
+		file_open(testinput, "Testinput.txt", read_mode);
+		file_open(testoutput, "Testoutput.txt", read_mode);
+		file_open(tb_output, "tb_output.txt", write_mode);
+		counter := 0;
 		
-		while not (endfile(infile)) loop
-		    readline(infile, INLine);	hread(INLine, pt128);
-			readline(infile, INLine);	hread(INLine, key128);
-			readline(infile, INLine);	hread(INLine, ct128);
-						
+		while not (endfile(testinput)) loop
+		    readline(testinput, line_var);	hread(line_var, pt128);
+			readline(testinput, line_var);	hread(line_var, key128);
+			readline(testoutput, line_var);	hread(line_var, ct128);
+			
+			counter := counter + 1;
+			
 			wait for 2*quarter;
 			-- load 
 			for i in 127 downto 0 loop
@@ -80,10 +98,11 @@ begin
 			tmp(0) := CTBit;
 			wait for 2*quarter;
 			
+			hwrite(line_var, tmp);	writeline(tb_output, line_var);
 			assert tmp = ct128 report "======>>> DOES NOT MATCH <<<======" severity failure;
-			report "succ + ";
+			report "vector # " & integer'image(counter) & ": passed";
 		end loop;
-		assert false report ">>> OK <<<" severity failure;
+		cont_flag <= false;
 		wait;
 	end process;
 end tb;
--- aes/test_vectors/gen.py
+++ aes/test_vectors/gen.py
@@ -7,7 +7,7 @@ import random
 def hex(abc):
     b = binascii.hexlify(abc)
     b = str(b, "UTF-8")
-    return b
+    return b.upper()
 
 def unhex(hx):
     return binascii.unhexlify(hx)
@@ -18,12 +18,23 @@ BLOCK_SIZE = 16
 k= "603DEB1015CA71BE2B73AEF0857D7781"
 pt="6BC1BEE22E409F96E93D7E117393172A"
 
+testinput = open("Testinput.txt", "w")
+testoutput = open("Testoutput.txt", "w")
+
+
+
 for j in range(100):
     cipher = AES.new(unhex(k))
     ct = hex(cipher.encrypt(unhex(pt)))
-    print(pt)
-    print(k)
-    print(ct)
+    testinput.write(pt+"\n")
+    testinput.write(k+"\n")
+    testoutput.write(ct+"\n")
+    #print(pt)
+    #print(k)
+    #print(ct)
     tmp = k
     k = ct
     pt = tmp
+
+testinput.close()
+testoutput.close()
