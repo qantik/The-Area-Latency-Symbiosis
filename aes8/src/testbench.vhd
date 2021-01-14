25b8a41c4525dc62367f561401f8e8333b04dbd2
Fatih <fatih.balli@epfl.ch>
2021-01-14 16:12:52 +0100

    Added makefile for aes8

--- aes8/src/testbench.vhd
+++ aes8/src/testbench.vhd
@@ -19,11 +19,12 @@ architecture tb of testbench is
 	constant clkphase: time:= 50 ns;
 	constant quarter : time := 25 ns;
 	
-	file infile, ansfile, outfile : TEXT;	
+	file testinput, testoutput, tb_output : TEXT;
 		
 	signal KeyBit, PTBit, CTBit : std_logic_vector(7 downto 0);	
-        signal Reset, Ready, ClkxC: std_logic;
+    signal Reset, Ready, ClkxC: std_logic;
 	constant total_cycles : integer := 16*11;
+	signal cont_flag : boolean := true;
 	
 	component AES 
        port ( 
@@ -42,26 +43,43 @@ begin
 
 	process
 	begin
-		ClkxC <= '1'; wait for clkphase;
-		ClkxC <= '0'; wait for clkphase;
+		if cont_flag then
+            ClkxC <= '1';
+            wait for clkphase;
+            ClkxC <= '0';
+            wait for clkphase;
+        else
+            wait;
+        end if;
 	end process;
 	
 	process
 	begin
-		Reset <= '0'; wait for quarter;
-		Reset <= '1'; wait for 2*total_cycles*clkphase - quarter;
+		if cont_flag then
+			Reset <= '0'; wait for quarter;
+			Reset <= '1'; wait for 2*total_cycles*clkphase - quarter;
+		else
+			wait;
+		end if;
 	end process;
 
 	process
-		variable INLine,mline : line;
+		variable line_var : line;
 		variable pt128, ct128, key128, tmp : std_logic_vector(127 downto 0);
+		variable counter  : integer;
 	begin
-		file_open(infile, "./test_vectors/PTKEY", read_mode);
+		cont_flag <= true;
+		file_open(testinput, "Testinput.txt", read_mode);
+		file_open(testoutput, "Testoutput.txt", read_mode);
+		file_open(tb_output, "tb_output.txt", write_mode);
+		counter := 0;
 		
-		while not (endfile(infile)) loop
-		    readline(infile, INLine);	hread(INLine, pt128);
-			readline(infile, INLine);	hread(INLine, key128);
-			readline(infile, INLine);	hread(INLine, ct128);
+		while not (endfile(testinput)) loop
+		    readline(testinput, line_var);	hread(line_var, pt128);
+			readline(testinput, line_var);	hread(line_var, key128);
+			readline(testoutput, line_var);	hread(line_var, ct128);
+			
+			counter := counter + 1;
 						
 			wait for 2*quarter;
 			-- load 
@@ -80,12 +98,12 @@ begin
 			
 			tmp(7 downto 0) := CTBit;
 			wait for 2*quarter;
-                        --hwrite(mline,tmp);
-                        --writeline(output,mline);
+
+			hwrite(line_var, tmp);	writeline(tb_output, line_var);                        
 			assert tmp = ct128 report "======>>> DOES NOT MATCH <<<======" severity failure;
-			report "succ + ";
+			report "vector # " & integer'image(counter) & ": passed";
 		end loop;
-		assert false report ">>> OK <<<" severity failure;
+		cont_flag <= false;
 		wait;
 	end process;
 end tb;
